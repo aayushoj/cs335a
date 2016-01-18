20b6b8f4cfd77a39556d8d60fdfb16fdacaf9264
Jain <shubhja@iitk.ac.in>
2016-01-18 21:48:46 +0530

    test files corrected and broken down

--- test/test1.java
+++ test/test1.java
@@ -2,12 +2,6 @@
 /*  The HelloWorldApp class implements an application that
   simply prints "Hello World!" to standard output.
  */
-class HelloWorldApp {
-    public static void main(String[] args) {
-        int system=2;
-        System.out.println("Hello World!"); // Display the string.
-    }
-}
 class MyClass {
    int he12ight;
    int height2;
--- test/test2.java
+++ test/test2.java
@@ -1,79 +1,15 @@
-// asdas
-/*  The HelloWorldApp class implements an application that
-  simply prints "Hello World!" to standard output.
- */
-class HelloWorldApp {
-    public static void main(String[] args) {
-        int system=2;
-        /*
-        shit this is a comment
-        *****I am jain ********/System.out.println("Hello World!"); // Display the string.
-    }
-}
-class  /*
-  shit this is a comment
-  *****I am jain ********/ MyClass {
-   int height;
-   MyClass() {
-      System.out.println("bricks");
-      height = 0;/*  The HelloWorldApp class implements an application that
-        simply prints "Hello World!" to standard output.
-       */
-   }
-   MyClass(  /*
-     shit this is a comment
-     *****I am jain ********/int i) {
-      System.out.println("Building new House that is "
-      + i + " feet tall");
-      height = i;
-      /*
-      shit this is a comment
-      *****I am jain ********/
-      public class MainClass {
-         public   /*
-           shit this is a comment
-           *****I am jain ********/static void main(String[] args) {
-            int nDisks = 3;
-            doTowers(nDisks,  /*
-              shit this is a comment
-              *****I am jain 2 ********/ 'A', 'B', 'C');
-         }
-         public static void doTowers(int topN, char from,
-         char  /*
-           shit this is a comment
-           *****I am jain ********/ inter, char to)  /*
-           shit this is a comment
-           *****I am jain 3 ********/ {
-            if (topN == 1){
-               System.out.println("Disk 1 from "
-               + from + " to " + to);
-            }else {
-               doTowers(topN - 1, from,  /*
-                 shit this is a comment
-                 *****I am jain ********/ to, inter);
-               System.out.println("Disk "
-               + topN + " from "   /*
-                 shit this is a comment
-                 *****I am jain 4 ********/+ from + " to " + to);
-               doTowers(topN - 1, inter,  /*
-                 shit this is a comment
-                 *****I am jain ********/ from, to);
-            }
-         }
-      }
+public class Factorial
+{
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
 
-      //*/
-   }
-   void info(  /*
-     shit this is a comment
-     *****I am jain ********/) {
-      System.out.println("House is " + height
-      + " feet tall")  /*
-        shit this is a comment
-        *****I am jain ********/;
-   }
-   void info(String s) {
-      System.out.println(s + ": House is "
-      + height + " feet tall");
-   }
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
 }
