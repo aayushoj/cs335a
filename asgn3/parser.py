91123156b1e342f8ffcc9cb9281e04070b9bba64
kmramit <amiitkumar1995@gmail.com>
2016-02-29 23:19:56 +0530

    some more grammar rules are added

--- asgn3/parser.py
+++ asgn3/parser.py
@@ -48,9 +48,78 @@ def p_PrimitiveType(p):
                 | KEYVOID
                 | KEYFLOAT
     '''
-# def p_ClassNameList(p):
+def p_ClassNameList(p):
+    '''ClassNameList : QualifiedName
+                 | ClassNameList SEPCOMMA QualifiedName
+    '''
+# def p_CompliationUnit(p):
+#     '''CompliationUnit : ProgramFile
 #     '''
+# def p_ProgramFile(p):
+#     ''' ProgramFile : PackageStatement ImportStatements TypeDeclarations
+#                 | PackageStatement ImportStatements
+#                 | PackageStatement TypeDeclarations
+#                 | ImportStatements TypeDeclarations
+#                 | PackageStatement
+#                 | ImportStatements
+#                 | TypeDeclarations
 #     '''
+# def p_PackageStatement(p):
+#     '''PackageStatement : PACKAGE QualifiedName Semicolons
+#     '''
+# def p_TypeDeclarations(p):
+#     '''TypeDeclarations : TypeDeclarationOptSemi
+#                     | TypeDeclarations TypeDeclarationOptSemi
+#     '''
+# def p_TypeDeclarationOptSemi(p):
+#     '''TypeDeclarationOptSemi : TypeDeclaration
+#                     | TypeDeclaration SemiColons
+#     '''
+# def p_TypeDeclaration(p):
+#     '''TypeDeclaration : ClassHeader SEPLEFTPARAN FieldDeclarations SEPRIGHTPARAN
+#                     | ClassHeader SEPLEFTPARAN SEPRIGHTPARAN
+#     '''
+# def p_ClassHeader(p):
+#     '''ClassHeader : Modifiers ClassWord Identifier Extends Interfaces
+#                 | Modifiers ClassWord Identifier Extends
+#                 | Modifiers ClassWord Identifier Interfaces
+#                 | ClassWord Identifier Extends Interfaces
+#                 | Modifiers ClassWord Identifier
+#                 | ClassWord Identifier Extends
+#                 | ClassWord Identifier Interfaces
+#                 | ClassWord Identifier
+#     '''
+def p_Modifiers(p):
+    '''Modifiers : Modifier
+                | Modifiers Modifier
+    '''
+def p_Modifier(p):
+    '''Modifier : KEYPUBLIC
+                | KEYPROTECTED
+                | KEYPRIVATE
+                | KEYSTATIC
+    '''
+def p_Statement(p):
+    '''Statement : EmptyStatement
+                | ExpressionStatement SEPSEMICOLON
+                | SelectionStatement
+                | IterationStatement
+                | JumpStatement
+                | Block
+    '''
+def p_EmptyStatement(p):
+    ''' EmptyStatement : SEPSEMICOLON
+    '''
+def p_ExpressionStatement(p):
+    '''ExpressionStatement : Expression
+    '''
+def p_SelectionStatement(p):
+    '''SelectionStatement : KEYIF SEPLEFTBRACE Expression SEPRIGHTBRACE Statement
+                        | KEYIF SEPLEFTBRACE Expression SEPRIGHTBRACE Statement KEYELSE Statement
+    '''
+def p_IterationStatement(p):
+    '''IterationStatement : KEYWHILE SEPLEFTBRACE Expression SEPRIGHTBRACE Statement
+    '''
 def p_error(p):
     if p == None:
         print "You missed something at the end"
