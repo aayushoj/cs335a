1c32b834d4ab28fdade342c0d8aed1661c7998b7
kmramit <amiitkumar1995@gmail.com>
2016-02-29 22:41:55 +0530

    some more grammar rules are added

--- asgn3/parser.py
+++ asgn3/parser.py
@@ -11,20 +11,46 @@ def p_program(p):
 
 def p_importstatements(p):
     '''importstatements : importstatement
-                    |importstatements importstatement
+                    | importstatements importstatement
     '''
 def p_importstatement(p):
     '''importstatement : KEYIMPORT QualifiedName Semicolons
-                    | KEYIMPORT QualifiedName '.' '*' Semicolons
+                    | KEYIMPORT QualifiedName SEPDOT OPMULTIPLY Semicolons
+    '''
+def p_QualifiedName(p):
+    '''QualifiedName : Identifier
+                | QualifiedName SEPDOT Identifier
     '''
 def p_Semicolons(p):
     '''Semicolons : SEPSEMICOLON
                 | Semicolons SEPSEMICOLON
     '''
-
-def p_methodline(p):
-    '''methodline : KEYPUBLIC KEYSTATIC KEYVOID Identifier '''
-
+def p_TypeSpecifier(p):
+    '''TypeSpecifier : TypeName               
+    '''
+#don't know what is Dims
+def p_Semicolons(p):
+    '''Semicolons : SEPSEMICOLON
+                | Semicolons SEPSEMICOLON
+    '''
+def p_TypeName(p):
+    '''TypeName : PrimitiveType
+            | QualifiedName
+    '''
+def p_PrimitiveType(p):
+    '''PrimitiveType : KEYBOOLEAN
+                | KEYCHAR
+                | KEYDOUBLE
+                | KEYBYTE
+                | KEYSHORT
+                | KEYINT
+                | KEYLONG
+                | KEYVOID
+                | KEYFLOAT
+    '''
+# def p_ClassNameList(p):
+#     '''
+#     '''
 def p_error(p):
     if p == None:
         print "You missed something at the end"
@@ -32,3 +58,11 @@ def p_error(p):
         print "Syntax error in input line!"
 
 parser = yacc.yacc()
+
+while 1:
+    try:
+        s = raw_input('calc > ')
+    except EOFError:
+        break
+    if not s: continue
+    yacc.parse(s)
