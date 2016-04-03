2d4ca756403a96daafe04b009c61c4e5b50c9741
Sourav Anand <sourav.anand14@gmail.com>
2016-04-04 01:24:58 +0530

    We have power of arrays now

--- asgn4/temp/test.java
+++ asgn4/temp/test.java
@@ -3,15 +3,23 @@ class amit{
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		int t,i;
-		t=7;
-		while(i<3)
-		{
-			t=8;
-			int y=0;
-		}
-		shubh();
-		y=8;
+		// int t,i;
+		// t=7;
+		// while(i<3)
+		// {
+		// 	t=8;
+		// 	int y=0;
+		// }
+		// shubh();
+		// y=8;
+		int[] a;
+		a = new int[3];
+		int i,j,k;
+		// int a,b,c;
+		// c*=a+=b;
+		// a[i*i]=a[ i+i*i/i] *a[i]+a[i*j]-a[j]/a[i];
+		while(j<=0 )
+			a[j]=a[i]|a[k];
 	}
 }
 }
