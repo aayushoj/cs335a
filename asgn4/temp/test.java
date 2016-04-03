0ce8cbfd903965da62f42fabb91a1333f8bf5bf1
kmramit <amiitkumar1995@gmail.com>
2016-04-04 02:09:10 +0530

    just for pulling

--- asgn4/temp/test.java
+++ asgn4/temp/test.java
@@ -2,16 +2,20 @@ import java.io.*;
 class amit{
 public class HelloWorld
 {
-	public static void main(String[] args) {
-		int t,i;
-		t=7;
-		while(i<3)
+	public static int shubh()
+	{
+		int d;
+		d=5;
+		if(d==5)
 		{
-			t=8;
-			int y=0;
+			return;
 		}
+		
+	}
+	public static void main(String[] args) {
+		int t,g;
+		t=7;
 		shubh();
-		y=8;
 	}
 }
 }
--- asgn4/temp/threeAddressCode.py
+++ asgn4/temp/threeAddressCode.py
@@ -1,42 +1,26 @@
 class ThreeAddressCode:
 
-	#Initialize
 	def __init__(self):
 		self.code = []
 		self.quad = -1
 		self.nextQuad = 0
 		self.labelBase = "l"
-		self.labelFunc = "f"
-		self.labelFuncNo = -1
 		self.labelNo = -1
 
-	#Add instruction to code list
 	def emit(self,dest, src1, src2, op):
 		self.code.append([dest,src1,src2,op])
 		self.quad += 1
 		self.nextQuad += 1
 	
-	#Get current Quad
-	def getCurrQuad(self):
-		return self.quad
-
-	#Print the code
 	def printCode(self):
 		for currInstr in self.code:
 			print currInstr
 
-	#Patch the goto statements
 	def patch(self, instrList, label):
 		for i in instrList :
 			if i < self.nextQuad  and self.code[i][0] == 'goto':
 				self.code[i][1] = label
 
-	#Make a new label
 	def makeLabel(self):
 		self.labelNo += 1
 		return self.labelBase + str(self.labelNo)
-
-	#Make a new function label
-	def makeFuncLabel(self):
-		self.labelFuncNo += 1
-		return self.labelFunc + str(self.labelFuncNo)
